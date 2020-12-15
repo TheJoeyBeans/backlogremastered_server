0b72f9a8b9b46d000d0362a0b424f137892218d6
Joseph Benbella <47164559+TheJoeyBeans@users.noreply.github.com>
2020-12-14 21:17:13 -0700

    Connected MySQL database with the backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
-router.get('/get', async (req, res) =>{
-    console.log("ROUTE GETTING HIT");
-    res.send('Found me');
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    database: "backlogremastered_database",
+    password: "Supersonic64!",
+    port: 3306,
+    multipleStatements: true
+});
+
+db.post("/register", (req, res) =>{
+    console.log(req.body);
+    console.log("HERE IS THE REQ BODY");
+    // db.query("INSERT INTO users (username, email, password) VALUES (?,?)", 
+    // [username, email, password], 
+    // (err, result) =>{
+    //     console.log(err);
+    // })
+})
+
+db.connect((err) =>{
+    if(!err){
+        console.log("Connected$$$$$$$$$");
+    }else{
+        console.log("Connection failed!!!!!!!!!!!!");
+        console.log(err, 'THE ERROR')
+    }
 });
 
 module.exports = router;
